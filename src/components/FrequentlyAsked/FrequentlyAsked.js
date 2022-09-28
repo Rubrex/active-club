7f590c121378cafb64cd0cf40e6da975a19719e0
Rubel Hossain <rubrex96@hotmail.com>
2022-09-29 05:08:15 +0600

    FAQ- answered

--- src/components/FrequentlyAsked/FrequentlyAsked.js
+++ src/components/FrequentlyAsked/FrequentlyAsked.js
@@ -5,15 +5,15 @@ const FrequentlyAsked = () => {
   const questions_ans = [
     {
       ques: "How does React work?",
-      ans: "dfgdfg sdf ggfhgfh dgfh dfg hdgf hfdg hdgf hdgf hdf ghd gfh dfg hdgf hdgf hdfs g sdfg sdf",
+      ans: "React is a javascript Library focused on WEB UI, It works by creating virtual DOM(Document Object Model). When state changes, it compares updated Virtual DOM with older Virtual DOM and applies the changed part to the actual DOM thus it makes Rendering more efficient. React also uses Diff Algorithm for optimizations.",
     },
     {
       ques: "What's the difference between props and state?",
-      ans: "dfgdfg sdf ggfhgfh dgfh dfg hdgf hfdg hdgf hdgf hdf ghd gfh dfg hdgf hdgf hdfs g sdfg sdf",
+      ans: "State and props are two different things. States are used to store values, It can store primitive, non primitive or functions values alltogether. Whereas Props are used to pass data from one Component to another Component.",
     },
     {
       ques: "What's the purpose of useEffect other than loading data?",
-      ans: "dfgdfg sdf ggfhgfh dgfh dfg hdgf hfdg hdgf hdgf hdf ghd gfh dfg hdgf hdgf hdfs g sdfg sdf",
+      ans: "useEffect is used to handle 'Side Effects' of a website. It can be used to manually change the DOM, cleanups to avoid memory leak etc.",
     },
   ];
   return (
