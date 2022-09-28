e99d01fb7a5a486ffd948eef2030d8ca9ada0504
Rubel Hossain <rubrex96@hotmail.com>
2022-09-29 03:42:36 +0600

    faq- questions added

--- src/components/FrequentlyAsked/FrequentlyAsked.js
+++ src/components/FrequentlyAsked/FrequentlyAsked.js
@@ -2,9 +2,30 @@ import React from "react";
 import QuestionAnswered from "./QuestionAnswered/QuestionAnswered";
 import "./FrequentlyAsked.css";
 const FrequentlyAsked = () => {
+  const questions_ans = [
+    {
+      ques: "How does React work?",
+      ans: "dfgdfg sdf ggfhgfh dgfh dfg hdgf hfdg hdgf hdgf hdf ghd gfh dfg hdgf hdgf hdfs g sdfg sdf",
+    },
+    {
+      ques: "What's the difference between props and state?",
+      ans: "dfgdfg sdf ggfhgfh dgfh dfg hdgf hfdg hdgf hdgf hdf ghd gfh dfg hdgf hdgf hdfs g sdfg sdf",
+    },
+    {
+      ques: "What's the purpose of useEffect other than loading data?",
+      ans: "dfgdfg sdf ggfhgfh dgfh dfg hdgf hfdg hdgf hdgf hdf ghd gfh dfg hdgf hdgf hdfs g sdfg sdf",
+    },
+  ];
   return (
-    <div className="faq-sections">
-      <QuestionAnswered />
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: "80px" }}>
+        Frequently Asked Questions w Answers
+      </h1>
+      <div className="faq-sections">
+        {questions_ans.map((quesAns) => (
+          <QuestionAnswered quesAns={quesAns} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/FrequentlyAsked/QuestionAnswered/QuestionAnswered.js
+++ src/components/FrequentlyAsked/QuestionAnswered/QuestionAnswered.js
@@ -1,7 +1,13 @@
 import React from "react";
-
-const QuestionAnswered = () => {
-  return <div>hello</div>;
+import "./QuestionAnswered.css";
+const QuestionAnswered = (props) => {
+  const { ques, ans } = props.quesAns;
+  return (
+    <div className="question-container">
+      <h2>{ques}</h2>
+      <p>{ans}</p>
+    </div>
+  );
 };
 
 export default QuestionAnswered;
